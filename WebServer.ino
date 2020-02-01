a1ee91c4277c4e9009d5cb69d28a786460614693
Christoph Birk <christoph.birk@gmail.com>
2020-02-01 18:01:12 +0100

    Errors reading from sensor

--- WebServer.ino
+++ WebServer.ino
@@ -1,6 +1,8 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+//#include <ESP8266WebServer.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <DHT.h>
 #include <Adafruit_Sensor.h>
 
@@ -13,9 +15,7 @@
 
 DHT dht(DHTPIN, DHTTYPE, 22);
 
-ESP8266WebServer server(80);
-
-
+AsyncWebServer server(80);
 
 float temperature, humidity;
 int soil = analogRead(A0);
@@ -23,18 +23,54 @@ float soil_percent;
 unsigned long previousMillis = 0;
 const long interval = 2000;
 
+String getTemperature(){
+  float temp = dht.readTemperature();
+  if(isnan(temp)){
+    return "error reading temp";
+  }
+  Serial.println("Temperature read: " + String(temp) + "°C");
+  return String(temp);
+}
+
+String getHumidity(){
+  float humidity = dht.readHumidity();
+  if(isnan(humidity)){
+    return "error reading humidity";
+  }
+  Serial.println("Humidity read: " + String(humidity) + "%");
+  return String(humidity);
+}
+
+String processor(const String& var){
+  Serial.println(var);
+  if (var == "TEMPERATURE"){
+    return getTemperature();
+  }
+  else if (var == "HUMIDITY"){
+    return getHumidity();
+  }
+  else {
+    Serial.println("Error: Case not defined");
+  }  
+}
+
 void setup() {
     Serial.begin(115200); 
     delay(10);
 
+    if(!SPIFFS.begin()){
+      Serial.println("An Error has occurred while mounting SPIFFS");
+      return;
+    }
 
     dht.begin();
-    humidity = dht.readHumidity();
-    temperature = dht.readTemperature();
+    String temp = String(dht.readTemperature());
+    String humidity = String(dht.readHumidity());
+    Serial.println("Temp: " + temp + "°C" + " Humidity: " + humidity + "%");
     pinMode(soil, INPUT);
-   
+    delay(10);
 
-    Serial.println(); Serial.println();
+    Serial.println();
     Serial.print("Connecting to ");
     Serial.println(WLAN_SSID);
 
@@ -47,46 +83,24 @@ void setup() {
     }
     
     Serial.println();
-
     Serial.println("WiFi connected");
-    Serial.println("IP address: "); Serial.println(WiFi.localIP());
+    Serial.println("IP address: "); Serial.println(WiFi.localIP());        
+     
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send(SPIFFS, "/index.html", String(), false, processor);
+    });
 
-    
-    server.on("/temp.json", [](){
-        unsigned long currentMillis = millis();
-
-        if (currentMillis - previousMillis >= interval) {
-            previousMillis = currentMillis;
-
-            humidity = dht.readHumidity();
-            temperature = dht.readTemperature();
-                     
-
-            if (isnan(humidity) || isnan(temperature)) {
-                Serial.println("Failed to read from DHT sensor!");
-                return;
-            }
-
-            Serial.println("Reporting " + String((int)temperature) + "C and " + String((int)humidity) + " % humidity");
-        }
-
-        StaticJsonDocument<500> doc;
-        doc["hostname"] = HOSTNAME;
-        doc["temperature"] = temperature;
-        doc["humidity"] = humidity;
-        
-       
-        String jsonString;
-        serializeJson(doc, jsonString);
-
-        Serial.println(jsonString);
-        server.send(200, "application/json", jsonString);
+    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send_P(200, "text/plain", getTemperature().c_str());
+    });
+  
+    server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send_P(200, "text/plain", getHumidity().c_str());
     });
 
     server.begin();
-    Serial.println("HTTP server started! Waiting for clients!");
+    Serial.println("Async HTTP server started! Waiting for clients!");
 }
 
 void loop() {
-    server.handleClient();
 }
